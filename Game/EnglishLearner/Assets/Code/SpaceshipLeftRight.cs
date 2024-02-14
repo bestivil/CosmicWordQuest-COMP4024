ed4e2f8e232844aecdcc5b3186e9a8204b17152d
bruce <psybe1@nottingham.ac.uk>
2024-02-14 12:14:14 +0000

    feat: spaceship movement using the translate function in C#

---
+++ Game/EnglishLearner/Assets/Code/SpaceshipLeftRight.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipLeftRight : MonoBehaviour
+{
+    public float moveSpeed = 5000f; // Speed of movement
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move spaceship if left arrow pressed
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+        }
+    }
+}
