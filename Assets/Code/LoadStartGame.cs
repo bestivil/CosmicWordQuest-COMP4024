11f70b7b31f028185239154e531c5d717e8ebeb7
Rodion Rasulov <psyrr4@nottingham.ac.uk>
2024-02-28 21:32:39 +0000

    Removed text buttons for actual buttons

--- Assets/Code/LoadStartGame.cs
+++ Assets/Code/LoadStartGame.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadStartGame : MonoBehaviour
 {
+    ToggleGroup toggleGroup;
+    
     void Update()
     {
         if (Input.GetMouseButtonDown(0)) // Check if the left mouse button was pressed
@@ -10,8 +13,19 @@ public class LoadStartGame : MonoBehaviour
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
             if (hit.collider != null && hit.collider.gameObject.CompareTag("StartButton")) // Check if 'WhiteButton' was clicked
             {
+
+                // will need code here to get the current language choice
+
+                toggleGroup = GetComponent<ToggleGroup>();
+
+
                 SceneManager.LoadScene("CosmicWordQuest"); // Load the scene
             }
         }
+    }
+    
+
+    public void onClick(){
+        SceneManager.LoadScene("CosmicWordQuest"); // Load the game scene
     }
 }
