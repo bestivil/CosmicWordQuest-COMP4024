8369f3c939ab18414b670909d96a1c5bdc88bd3e
Rodion Rasulov <psyrr4@nottingham.ac.uk>
2024-03-05 12:30:02 +0000

    Refactor back button code, implement restart game button in LeaderboardScene

--- Assets/Code/LoadMenu.cs
+++ Assets/Code/LoadMenu.cs
@@ -3,16 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class LoadMenu : MonoBehaviour
 {
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0)) // Check if the left mouse button was pressed
-        {
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-            if (hit.collider != null && hit.collider.gameObject.CompareTag("BackButton")) 
-            {
-                SceneManager.LoadScene("StartScreen"); // Load the scene
-            }
-        }
+    public void OnClick(){
+        SceneManager.LoadScene("StartScreen");
     }
 }
 
