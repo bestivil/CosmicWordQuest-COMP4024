84b8c78e048e2d9dd67f56bd7a54188fa6b64db9
bruce <psybe1@nottingham.ac.uk>
2024-03-06 00:32:20 +0100

    fix script and change to load correct scene

--- Assets/Code/LoadMenu.cs
+++ Assets/Code/LoadMenu.cs
@@ -3,8 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class LoadMenu : MonoBehaviour
 {
-    public void OnClick(){
-        SceneManager.LoadScene("StartScreen");
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // Check if the left mouse button was pressed
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit.collider != null && hit.collider.gameObject.CompareTag("BackButton"))
+            {
+                SceneManager.LoadScene("StartScreen"); // Load the start menu
+            }
+        }
     }
 }
+
 
