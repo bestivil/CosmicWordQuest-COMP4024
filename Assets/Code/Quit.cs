a79f33013cef935236d0dfd55c4099955be75e6f
Rodion Rasulov <psyrr4@nottingham.ac.uk>
2024-03-06 14:23:03 +0000

    fix: get Quit button working in editor mode

--- Assets/Code/Quit.cs
+++ Assets/Code/Quit.cs
@@ -1,15 +1,22 @@
 using UnityEngine;
-
+using UnityEditor;
 /// <summary>
 /// Class responsible for Quit button behaviour
 /// </summary>
 public class Quit : MonoBehaviour
 {   
     /// <summary>
-    /// Quits the application
+    /// Quits the application, if it is in editor mode it stops it playing too
     /// </summary>
     public void QuitGame()
     {
         Application.Quit();
+        #if UNITY_EDITOR
+            if (EditorApplication.isPlaying){
+                EditorApplication.isPlaying = false;
+            }
+        #else
+            Application.Quit();
+        #endif
     }
 }
