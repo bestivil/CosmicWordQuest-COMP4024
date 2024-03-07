ac59feacaf1520b195d5db045287c87800e14699
HsiLucas <psxlh7@nottingham.ac.uk>
2024-03-07 00:35:46 +0000

    minor refactoring

--- Assets/Code/LeaderboardManager.cs
+++ Assets/Code/LeaderboardManager.cs
@@ -14,8 +14,7 @@ public class LeaderboardManager : MonoBehaviour
     public PlayerDataList playerList; //stores an array of PlayerData
 
     string csvFilePath;
-    //public TextAsset CSVData = Application.persistentDataPath + "/ScoresCSV.csv";
-
+    
     public GameObject[] highscoreTexts = new GameObject[5];
 
     public PlayerData[] sortedList;
@@ -57,24 +56,6 @@ public class LeaderboardManager : MonoBehaviour
                 score = int.Parse(fields[1])
             };
         }
-
-        //get the data in that file and split it by commas and new lines
-
-       // string[] data = CSVData.text.Split(new string[] { ",", "\n"},StringSplitOptions.None);
-
-        //determine how wide the data table is
-        // 2 columns, and ignore first row as its just columns
-        //int dataTableSize = data.Length / 2 - 1;
-
-        //playerList.players = new PlayerData[dataTableSize];
-
-       // for(int i=0; i < dataTableSize; i++){
-        //    playerList.players[i] = new PlayerData
-        //    {
-        //        name = data[2 * (i + 1)], // get the name by this maths
-        //        score = int.Parse(data[2 *(i+1) + 1]) // score is the next part of the data entry
-        //    };
-        //}
         
         PlayerData[] sortedList = SelectTop5();
         
